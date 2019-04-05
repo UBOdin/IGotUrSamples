93af4753808adfefa30423c81df093c504d4591a
Blake Cooper <contact@blake-cooper.com>
2019-04-05 15:11:53 -0400

    Building "add samples" page

--- sam-database/src/components/Addsamples-aliquots.jsx
+++ sam-database/src/components/Addsamples-aliquots.jsx
@@ -3,22 +3,21 @@ import { Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
 import './Addsamples.css';
 import { Link } from 'react-router-dom';
 
-class Addsamples extends Component {
+class Addsamples-aliquots extends Component {
   render() {
     return (
       <div>
-        <h2>New sample:</h2>
         <Container>
             <Row>
                 <Col>
                     <InputGroup className="mb-3">
                     <InputGroup.Prepend>
-                        <InputGroup.Text id="id">ID</InputGroup.Text>
+                        <InputGroup.Text id="aliquots">Aliquots</InputGroup.Text>
                     </InputGroup.Prepend>
                     <FormControl
-                        placeholder="Sample ID"
-                        aria-label="Sample ID"
-                        aria-describedby="basic-addon1"
+                        placeholder=""
+                        aria-label=""
+                        aria-describedby=""
                     />
                     </InputGroup>
                     <InputGroup className="mb-3">
@@ -82,4 +81,5 @@ class Addsamples extends Component {
 }
 
 export default Addsamples;
+
 
--- sam-database/src/components/Navbar.jsx
+++ sam-database/src/components/Navbar.jsx
@@ -1,30 +1,18 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
 import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
 
 class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <h1>
-            SAM Research Database
-        </h1>
-        <Button bsStyle="secondary">
-            Add Samples
-        </Button>
-        <Button bsStyle="secondary">
-            View Samples
-        </Button>
-        <Button bsStyle="secondary">
-            Add Shipment
-        </Button>
-        <Button bsStyle="secondary">
-            View Shipments
-        </Button>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Navbar>
+				<Nav pullRight>
+					<Nav.Link href="/help">Help</Nav.Link>
+					<Nav.Link href="/login">Log in</Nav.Link>
+				</Nav>
+			</Navbar>
+		)
+	}
 }
 
 export default Navbar;
