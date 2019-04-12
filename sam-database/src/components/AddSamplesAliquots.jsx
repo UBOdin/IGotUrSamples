f8b315c988002e9868b6c42d73636ac745287e59
blakecooper <accounts@blake-cooper.com>
2019-04-11 21:16:28 -0400

    AddSamples changes from Dr. Kordas

--- sam-database/src/components/AddSamplesAliquots.jsx
+++ sam-database/src/components/AddSamplesAliquots.jsx
@@ -5,65 +5,7 @@ class AddSamplesAliquots extends Component {
   render() {
     return (
       <div>
-            <hr />
-            <Row>
-                <Col>
-                    <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                        <InputGroup.Text>Type:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <Form.Control id="type" as="select" >
-                        <option>Blood</option>
-                        <option>Blood Spot</option>
-                        <option>Dust</option> 
-                        <option>Hair</option>
-                        <option>Plasma</option>
-                        <option>Serum</option>
-                        <option>Soil</option>
-                        <option>Urine</option>
-                        <option>Water</option>
-                    </Form.Control>
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                        <InputGroup.Text>Aliquots:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl id="aliquots" />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                        <InputGroup.Text>Initial storage conditions:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <Form.Control id="initialstorageconditions" as="select">
-                        <option>Room temperature</option>
-                        <option>4° C</option>
-                        <option>-20° C</option>
-                        <option>-80° C</option>
-                    </Form.Control>
-                    </InputGroup>
-                </Col>
-                <Col>
-                    <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                        <InputGroup.Text>Treatments:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <Form.Control id="treatments" as="select" multiple>
-                        <option>BHT</option>
-                        <option>EDTA</option>
-                        <option>Foil Wrapping</option>
-                        <option>Heparin</option>
-                        <option>MPA</option>
-                    </Form.Control>
-                    </InputGroup>
-                    <InputGroup>
-                    <InputGroup.Prepend>
-                        <InputGroup.Text>Other treatments:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <Form.Control id="othertreatments" />
-                    </InputGroup>
-                </Col>
-            </Row>
-      </div>
+     </div>
     );
   }
      
