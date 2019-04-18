4c24ba7eca7542cc79fdca49fb8e21ad4ac4ea33
Blake <contact@blake-cooper.com>
2019-04-18 13:30:10 -0400

    troubleshooting connection

--- sam-database/scripts/connect.php
+++ sam-database/scripts/connect.php
@@ -1,20 +1,20 @@
 <?php
 
-	$host = $_GET["host"];
-	$user = $_GET["user"];
-	$password = $_GET["password"];
-	$db_name = $_GET["db_name"];
+//	$host = $_GET["host"];
+//	$user = $_GET["user"];
+//	$password = $_GET["password"];
+//	$db_name = $_GET["db_name"];
 
-	echo "This message appears whether successfully connected or not.";
+//	echo "This message appears whether successfully connected or not.";
 
-	$conn = mysqli_connect("tethys.cse.buffalo.edu","blakecoo","ChangeMe","eehuruguayresearch_db",3306);
+	$conn = new mysqli("tethys.cse.buffalo.edu","blakecoo","fiddle protrude repost barbeque","eehuruguayresearch_db",3306);
 
-	echo mysqli_connect_errno() . mysqli_connect_error();
-
-	if (!$conn) {
-		echo "MySQL connect error";
-	} else {
-
-	echo "connect success" . "<br>" . "<br>";
-	}
+//	echo mysqli_connect_errno() . mysqli_connect_error();
+//	echo "This comes after.";
+//	if (!$conn) {
+//		echo "MySQL connect error";
+//	} else {
+//
+//	echo "connect success" . "<br>" . "<br>";
+//	}
 ?>
